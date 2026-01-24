a69dd85e4d32f2de9ef28931498302c62856ff8d
codeAmyth <dev.amyth27@gmail.com>
2026-01-24 14:27:29 +0530

    setting up eureka server

--- eureka/src/main/java/com/fitness/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/fitness/eureka/EurekaApplication.java
@@ -2,7 +2,9 @@ package com.fitness.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
