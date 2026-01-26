bdf6ba9b1cb95e15cdad6c8cf99c0cf95ae2b2a1
codeAmyth <dev.amyth27@gmail.com>
2026-01-26 16:20:36 +0530

    ai recommendation generated and store into database

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -1,6 +1,8 @@
 package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,13 +12,20 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ActivityMessageListener {
 
-    private static final Logger log =
-            LoggerFactory.getLogger(ActivityMessageListener.class);
+//    private static final Logger log =
+//            LoggerFactory.getLogger(ActivityMessageListener.class);
+
+    private final ActivityAIService activityAIService;
+    private final RecommendationRepository recommendationRepository;
 
     @KafkaListener(topics = "${kafka.topic.name}", groupId = "activity-processor-group")
     public void processActivity(Activity activity) {
         log.info("Received Activity for processing: {}", activity.getUserId());
+        Recommendation recommendation = activityAIService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
     }
 }
